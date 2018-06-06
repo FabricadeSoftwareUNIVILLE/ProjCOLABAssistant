db3fe55b65667a08678a1dab1592a3730921a159
MARCUS DE MELO MOREIRA <marcus_de_melo_moreira@hotmail.com>
2018-06-06 20:29:18 -0300

    mo1

--- src/main/java/br/univille/projcolabassistant/controller/InstituicaoController.java
+++ src/main/java/br/univille/projcolabassistant/controller/InstituicaoController.java
@@ -5,11 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
+@Controller
 public class InstituicaoController {
 
-
-@Controller
 @RequestMapping("/instituicao")
 
 @GetMapping("")
