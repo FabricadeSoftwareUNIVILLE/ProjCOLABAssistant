5668dfa4a43b1aa287374fd0cf45701c9825d31f
STEFFANY CAMILA MATTEI CRUZ <steffany.cruz@univille.br>
2018-06-18 21:55:45 -0300

    repositorio

--- src/main/java/br/univille/projcolabassistant/controller/UserController.java
+++ src/main/java/br/univille/projcolabassistant/controller/UserController.java
@@ -1,5 +1,22 @@
 package br.univille.projcolabassistant.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/usuario")
 public class UserController {
+	
+	
+	@GetMapping("")
+    public ModelAndView index() {
+		
+		
+		return new ModelAndView("user/index");
+	}
+	
+	
 
 }
