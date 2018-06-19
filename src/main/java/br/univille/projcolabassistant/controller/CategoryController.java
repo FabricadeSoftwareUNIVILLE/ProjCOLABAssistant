40f6033f5c557776560917821261d1a66b9ce912
SABRINA BOING MOREIRA <sabrinamoreira@univille.br>
2018-06-18 22:02:57 -0300

    Primeira parte

---
+++ src/main/java/br/univille/projcolabassistant/controller/CategoryController.java
@@ -0,0 +1,17 @@
+package br.univille.projcolabassistant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/consultecategory")
+
+public class CategoryController {
+	@GetMapping("")
+	public ModelAndView index() {
+
+	return new ModelAndView("consultecategory/index");
+	}
+}
