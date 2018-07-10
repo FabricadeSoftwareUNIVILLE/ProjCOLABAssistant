0152d11a2c5e6f87c52368937689f5dee36a66bf
STEFFANY CAMILA MATTEI CRUZ <steffany.cruz@univille.br>
2018-07-09 22:21:58 -0300

    Listar

---
+++ src/main/java/br/univille/projcolabassistant/repository/CityRepository.java
@@ -0,0 +1,11 @@
+package br.univille.projcolabassistant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.univille.projcolabassistant.model.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long>{
+
+}
--- src/test/java/br/univille/projcolabassistant/ProjColabAssistantApplicationTests.java
+++ src/test/java/br/univille/projcolabassistant/ProjColabAssistantApplicationTests.java
@@ -1,16 +1,25 @@
 package br.univille.projcolabassistant;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import br.univille.projcolabassistant.controller.UserController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjColabAssistantApplicationTests {
 
+	@Autowired
+	private UserController controller;
+	
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
