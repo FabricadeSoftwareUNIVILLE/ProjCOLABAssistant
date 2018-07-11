57a9d896634947c739251c86fa3a854b68794ee3
Geórgia Spirandeli <georgia.spirandeli@univille.br>
2018-07-11 20:07:26 -0300

    Commit 11/07/2016

--- src/test/java/br/univille/projcolabassistant/ProjColabAssistantApplicationTests.java
+++ src/test/java/br/univille/projcolabassistant/ProjColabAssistantApplicationTests.java
@@ -1,16 +1,31 @@
 package br.univille.projcolabassistant;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ProjColabAssistantApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void consultAccessories() throws Exception {
+		this.mockMvc.perform(get("/catalogo")).andDo(print()).andExpect(status().isOk());
+	}
 }
