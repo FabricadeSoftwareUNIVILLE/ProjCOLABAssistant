99ea48f8d2afb1aa5612678712bcb9ebf76c872e
RAYSSA GABRIELI DOS SANTOS <rayssa.santos@univille.br>
2018-07-11 20:43:45 -0300

    finalização de trabalho category Sabrina, Anne e Rayssa

--- src/main/java/br/univille/projcolabassistant/model/Category.java
+++ src/main/java/br/univille/projcolabassistant/model/Category.java
@@ -1,12 +1,20 @@
 package br.univille.projcolabassistant.model;
 
 import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class Category {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotNull
+    @Length(min=2, max=1000, message="O tamanho do nome deve ser entre {min} e {max}")
     @Column(length = 500)
     private String name;
 
