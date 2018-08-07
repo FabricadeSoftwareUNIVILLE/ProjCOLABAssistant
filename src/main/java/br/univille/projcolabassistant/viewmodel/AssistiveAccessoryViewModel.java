e1d57f51b5df7b79604391c984704fadf3ff6214
Geórgia Spirandeli <georgia.spirandeli@univille.br>
2018-08-06 21:39:48 -0300

    Commit revisão de Issues

---
+++ src/main/java/br/univille/projcolabassistant/viewmodel/AssistiveAccessoryViewModel.java
@@ -0,0 +1,34 @@
+package br.univille.projcolabassistant.viewmodel;
+
+import br.univille.projcolabassistant.model.AccessoryColor;
+import br.univille.projcolabassistant.model.AssistiveAccessory;
+
+public class AssistiveAccessoryViewModel {
+	private AssistiveAccessory assistiveAccessory;
+	private AccessoryColor accessoryColor;
+	
+	public AssistiveAccessoryViewModel() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public AssistiveAccessoryViewModel(AssistiveAccessory assistiveAccessory,AccessoryColor accessoryColor) {
+		this.assistiveAccessory = assistiveAccessory;
+		this.accessoryColor = accessoryColor;
+	}
+	
+	
+	public AssistiveAccessory getAssistiveAccessory() {
+		return assistiveAccessory;
+	}
+	public void setAssistiveAccessory(AssistiveAccessory assistiveAccessory) {
+		this.assistiveAccessory = assistiveAccessory;
+	}
+	public AccessoryColor getAccessoryColor() {
+		return accessoryColor;
+	}
+	public void setAccessoryColor(AccessoryColor accessoryColor) {
+		this.accessoryColor = accessoryColor;
+	}
+	
+	
+}
