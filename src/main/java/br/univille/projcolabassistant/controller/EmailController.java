2b78afee6b548b41742ac7a3472c6018ada8bcd9
luan-fernando <luan.fernando@univille.br>
2018-08-07 01:02:07 +0000

    inicio funcao de enviar emails

---
+++ src/main/java/br/univille/projcolabassistant/controller/EmailController.java
@@ -0,0 +1,36 @@
+package br.univille.projcolabassistant.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class EmailController {
+    @Autowired private JavaMailSender mailSender;
+
+
+    @RequestMapping(path = "/enviar_email", method = RequestMethod.GET)
+    public String sendMail() {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setText("Test Spring Boot Application");
+        message.setTo("luan.fernando@univille.br");
+        message.setFrom("teste@gmail.com");
+        message.setSubject("Teste spring boot");
+
+
+        try {
+            mailSender.send(message);
+            return "Email enviado com sucesso!";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Erro ao enviar email.";
+        }
+    }
+
+}
