86c2ed52de00057f64d101519f51e61f12ded1b9
ERICK RICARDO BUDAL <erick.budal@univille.br>
2018-08-13 21:37:53 -0300

    Criado outros repositories relacionado aos repositórios

--- src/main/java/br/univille/projcolabassistant/repository/AssistiveAccessoryRepository.java
+++ src/main/java/br/univille/projcolabassistant/repository/AssistiveAccessoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.univille.projcolabassistant.model.AssistiveAccessory;
 
 public interface AssistiveAccessoryRepository extends JpaRepository<AssistiveAccessory, Long> {
-
+	
 }
