881ea5b24ba3daeb2a2940804674668c90cf16f4
LUCAS RYUICHI MARTINEZ NAKAGAWA <lucasnakagawa@univille.br>
2018-08-27 22:07:11 -0300

    Corrigido upload de arquivo

--- src/main/java/br/univille/projcolabassistant/controller/AssistiveAccessoryController.java
+++ src/main/java/br/univille/projcolabassistant/controller/AssistiveAccessoryController.java
@@ -31,81 +31,85 @@ import br.univille.projcolabassistant.repository.CategoryRepository;
 @RequestMapping("/assistiveaccessory")
 public class AssistiveAccessoryController {
 	@Autowired
-    private AssistiveAccessoryRepository accessoryRepository;
+	private AssistiveAccessoryRepository accessoryRepository;
 	@Autowired
-    private CategoryRepository categoryRepository;
-	
-	private static String UPLOADED_FOLDER = "image/assistiveaccessory";
-	
-    @GetMapping("")
-    public ModelAndView index() {
-    	List<AssistiveAccessory> accessoriesList = this.accessoryRepository.findAll();
-        
-        return new ModelAndView("assistiveaccessory/index","assistiveaccessories",accessoriesList);
-    }
-    
-    @GetMapping("/new")
-    public ModelAndView createForm(@ModelAttribute AssistiveAccessory assistiveaccessory) {
-    	List<Category> categories = categoryRepository.findAll();
-    	
-    	HashMap<String, Object> dados = new HashMap<String, Object>();
-    	dados.put("assistiveaccessory", assistiveaccessory);
-    	dados.put("categories",categories);
-    	
-        return new ModelAndView("assistiveaccessory/form",dados);
-    }
-    
-    @PostMapping(value="/new")
-    public ModelAndView save(@Valid AssistiveAccessory assistiveaccessory, BindingResult result, RedirectAttributes redirect) {
-        
-    	assistiveaccessory = this.accessoryRepository.save(assistiveaccessory);
-        
-        return new ModelAndView("redirect:/assistiveaccessory");
-    }
-    
-    @GetMapping(value="/update/{id}")
-    public ModelAndView alterarForm(@PathVariable("id") AssistiveAccessory assistiveaccessory) {
-    	List<Category> categories = categoryRepository.findAll();
-    	
-    	HashMap<String, Object> dados = new HashMap<String, Object>();
-    	dados.put("assistiveaccessory", assistiveaccessory);
-    	dados.put("categories",categories);
-    	
-        return new ModelAndView("assistiveaccessory/form",dados);
-    }
-    
-    @GetMapping(value="delete/{id}")
-    public ModelAndView remover(@PathVariable ("id") Long id, RedirectAttributes redirect) {
-        this.accessoryRepository.deleteById(id);
-        return new ModelAndView("redirect:/assistiveaccessory");
-    }
+	private CategoryRepository categoryRepository;
 
-    @PostMapping("/upload")
-    public String singleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
+	private static String UPLOADED_FOLDER = "C:\\ProjCOLABAssistant\\src\\main\\resources\\static\\image\\assistiveaccessory\\";
 
-        if (file.isEmpty()) {
-            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
-            return "redirect:uploadStatus";
-        }
+	@GetMapping("")
+	public ModelAndView index() {
+		List<AssistiveAccessory> accessoriesList = this.accessoryRepository.findAll();
 
-        try {
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            Files.write(path, bytes);
+		return new ModelAndView("assistiveaccessory/index", "assistiveaccessories", accessoriesList);
+	}
 
-            redirectAttributes.addFlashAttribute("message",
-                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+	@GetMapping("/new")
+	public ModelAndView createForm(@ModelAttribute AssistiveAccessory assistiveaccessory) {
+		List<Category> categories = categoryRepository.findAll();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		HashMap<String, Object> dados = new HashMap<String, Object>();
+		dados.put("assistiveaccessory", assistiveaccessory);
+		dados.put("categories", categories);
 
-        return "redirect:/uploadStatus";
-    }
+		return new ModelAndView("assistiveaccessory/form", dados);
+	}
 
-    @GetMapping("/uploadStatus")
-    public String uploadStatus() {
-        return "uploadStatus";
-    }
+	@PostMapping(value = "/new")
+	public ModelAndView save(@RequestParam("accessoryImage") MultipartFile file, @Valid AssistiveAccessory assistiveaccessory,
+			BindingResult result, RedirectAttributes redirect) {
+
+		assistiveaccessory = this.accessoryRepository.save(assistiveaccessory);
+
+		try {
+			byte[] bytes = file.getBytes(); 
+			Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+			Files.write(path, bytes);
+
+			redirect.addFlashAttribute("message", "Arquivo '" + file.getOriginalFilename() + "' enviado com sucesso!");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return new ModelAndView("redirect:/assistiveaccessory");
+	}
+
+	@GetMapping(value = "/update/{id}")
+	public ModelAndView alterarForm(@PathVariable("id") AssistiveAccessory assistiveaccessory) {
+		List<Category> categories = categoryRepository.findAll();
+
+		HashMap<String, Object> dados = new HashMap<String, Object>();
+		dados.put("assistiveaccessory", assistiveaccessory);
+		dados.put("categories", categories);
+
+		return new ModelAndView("assistiveaccessory/form", dados);
+	}
+
+	@GetMapping(value = "delete/{id}")
+	public ModelAndView remover(@PathVariable("id") Long id, RedirectAttributes redirect) {
+		this.accessoryRepository.deleteById(id);
+		return new ModelAndView("redirect:/assistiveaccessory");
+	}
+
+	/*
+	 * @PostMapping("/upload") public String singleFileUpload(@RequestParam("file")
+	 * MultipartFile file, RedirectAttributes redirectAttributes) {
+	 * 
+	 * if (file.isEmpty()) { redirectAttributes.addFlashAttribute("message",
+	 * "Please select a file to upload"); return "redirect:uploadStatus"; }
+	 * 
+	 * try { byte[] bytes = file.getBytes(); Path path = Paths.get(UPLOADED_FOLDER +
+	 * file.getOriginalFilename()); Files.write(path, bytes);
+	 * 
+	 * redirectAttributes.addFlashAttribute("message", "You successfully uploaded '"
+	 * + file.getOriginalFilename() + "'");
+	 * 
+	 * } catch (IOException e) { e.printStackTrace(); }
+	 * 
+	 * return "redirect:/uploadStatus"; }
+	 * 
+	 * @GetMapping("/uploadStatus") public String uploadStatus() { return
+	 * "uploadStatus"; }
+	 */
 }
