0db1e23a61e49531e27a70c50f56ec66c054291b
STEFFANY CAMILA MATTEI CRUZ <steffany.cruz@univille.br>
2018-09-10 22:07:13 -0300

    quase

---
+++ src/main/java/br/univille/projcolabassistant/repository/AccessoryPhotoRepository.java
@@ -0,0 +1,9 @@
+package br.univille.projcolabassistant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.univille.projcolabassistant.model.AccessoryPhoto;
+
+public interface AccessoryPhotoRepository extends JpaRepository<AccessoryPhoto, Long>{
+
+}
