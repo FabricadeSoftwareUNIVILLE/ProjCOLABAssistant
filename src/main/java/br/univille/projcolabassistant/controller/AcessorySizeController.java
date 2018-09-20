6a8c80bf834cb1da9f7f8fa9b4480ef0d2b35117
Walter Silvestre Coan <walter.coan@gmail.com>
2018-09-20 16:36:57 -0300

    correcao de diversos erros no cadastro de tamanhos

--- src/main/java/br/univille/projcolabassistant/controller/AcessorySizeController.java
+++ src/main/java/br/univille/projcolabassistant/controller/AcessorySizeController.java
@@ -1,17 +1,18 @@
 package br.univille.projcolabassistant.controller;
 import java.util.HashMap;
+
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -19,6 +20,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.univille.projcolabassistant.model.AccessorySize;
 
 import br.univille.projcolabassistant.repository.AccessorySizeRepository;
+@Controller
+@RequestMapping("/accessorysize")
 public class AcessorySizeController {
    @Autowired
    private AccessorySizeRepository accessorysizeRepository;
